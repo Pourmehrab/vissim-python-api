549ab1bac0634fc5ed12f0a6c1dc222346a5b2a2
Pourmehrab <mpourmehrab@ufl.edu>
2017-06-27 23:10:12 -0400

    vehicle attributes can be read and written

--- MATLAB/EXECUTE.m
+++ MATLAB/EXECUTE.m
@@ -11,7 +11,7 @@ f = filesep;
 addpath(genpath([p f 'trajectory-optimizer']),...
     genpath([p f 'traffic-sim']),genpath([p f 'VISSIM-MATLAB']));
 
-
+load('VissimData.mat');
 
 %% Connecting the COM Server => Open a new Vissim Window:
 Vissim = actxserver('Vissim.Vissim-64.800');
@@ -35,27 +35,54 @@ Vissim.LoadLayout(Filename);
 % Network Configuration
 %==========================================================================
 
-adjustNetwork
+adjustNetwork;
+
+
+initialSimParam;
 
 %% ========================================================================
-% Simulation Parameters
+% Signal Controller Interface
 %==========================================================================
 
-initialSimParam
-
-%% ========================================================================
-% Signal Controller
-%==========================================================================
-
-greenSGs = [7,2,4];
+greenSGs = [ 2, 4, 7 ];
 
 switchSignal( Vissim, 1, greenSGs )
 
+%% ========================================================================
+% Vehicle Interface
+%==========================================================================
+All_Vehicles    = Vissim.Net.Vehicles.GetAll; % get all vehicles in the network at the actual simulation second
+Vehicle         = All_Vehicles{1};
+
+% get link, lane, distance, type, speed for an specific ID: here 'No'
+get(All_Vehicles{1}.Lane,'AttValue','Link');
+get(All_Vehicles{1},'AttValue','Lane');
+get(All_Vehicles{1},'AttValue','Pos');
+get(All_Vehicles{1},'AttValue','VehType');
+get(All_Vehicles{1},'AttValue','Speed');
+
+Vissim.Simulation.SimulationSecond
+get(All_Vehicles{1},'AttValue','No');
+
+All_Vehicles{1}.NextLink
+% Set desired speed to a vehicle:
+new_desspeed = 30;
+set(Vehicle, 'AttValue', 'DesSpeed', new_desspeed);
+
+% Move a vehicle:
+link_number     = 17;
+lane_number     = 1;
+link_coordinate = 910;
+Vehicle.MoveToLinkPosition(link_number, lane_number, link_coordinate); % This function will operate during the next simulation step
+% Hint: In earlier Vissim releases, the name of the function was: MoveToLinkCoordinate
+
 Vissim.Simulation.RunSingleStep;
+
+display('done.')
+
 
 
 %% ========================================================================
 % End Vissim
 %==========================================================================
 Vissim.release
-display('done.')
--- MATLAB/VISSIM-MATLAB/initialSimParam.m
+++ MATLAB/VISSIM-MATLAB/initialSimParam.m
@@ -1,8 +1,9 @@
 %% Modify the Simulation Parameters
+commRange = 500;
 
 % Chose Random Seed
-Random_Seed = randi(1000);
-set(Vissim.Simulation, 'AttValue', 'RandSeed', Random_Seed);
+% Random_Seed = randi(1000);
+% set(Vissim.Simulation, 'AttValue', 'RandSeed', Random_Seed);
 
 % Set a signal controller program:
 SC_number = 1; % SC = SignalController
@@ -18,6 +19,20 @@ set(Vissim.Simulation, 'AttValue', 'UseMaxSimSpeed', true);
 % Hint: to change the speed use: set(Vissim.Simulation, 'AttValue', 'SimSpeed', 10); % 10 => 10 Sim. sec. / s
 % set simulation resolution to a small number to have nice and smooth
 % signal swithes
-set(Vissim.Simulation, 'AttValue', 'SimRes', 0.5);
+set(Vissim.Simulation, 'AttValue', 'SimRes', 10);
 
-Vissim.Simulation.RunSingleStep;
+% Vissim.Simulation.RunSingleStep;
+
+
+Sim_break_at = 200; % simulation second [s]
+set(Vissim.Simulation, 'AttValue', 'SimBreakAt', Sim_break_at);
+Vissim.Simulation.RunContinuous; % Start the simulation until SimBreakAt (200s)
+
+
+% Extract link lenghts from Vissim model
+VissimData.OptRange = zeros(size(VissimData.NumLanes));
+
+for ii = 1:VissimData.NumLinks
+    VissimData.OptRange(ii) = get(Vissim.Net.Links.ItemByKey(ii),'AttValue'...
+        ,'length2D') - commRange ;
+end
